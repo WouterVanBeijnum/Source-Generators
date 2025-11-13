94c6c4c0616634bf9c86eecc9c1a30347c911365
Wouter van Beijnum <Wouter.vanBeijnum@infosupport.com>
2025-11-13 20:39:48 +0100

    Scaffold Lab2c solution

---
+++ labs/2c. Schema to code/Lab2c/SourceGenerators/SchemaSourceGenerator.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace SourceGenerators;
+
+[Generator]
+public class SchemaSourceGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+}
